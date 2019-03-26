2af8fc660a4a1fbd273fdd5adad79a23a6ffbaec
Nobuyuki Tanabe <tanabe.1478@gmail.com>
2019-03-26 23:53:02 +0900

    component を Container component と Presentational component に分離

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import { createStore } from "redux";
 import reducer from "./reducer";
 
 import { Provider } from "react-redux";
-
-import App from "./App";
-
+import App from "./containers/App";
 const store = createStore(reducer);
 console.log(store);
 // storeの更新が確認されたときに呼ばれる関数
